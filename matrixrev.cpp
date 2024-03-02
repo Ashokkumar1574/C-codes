84bcab024a5c312e24fe6271c509f595ad25fcfa
Ashokkumar <152209063+Ashokkumar1574@users.noreply.github.com>
2024-03-02 13:43:08 +0530

    Matrux Reverse

---
+++ matrixrev.cpp
@@ -0,0 +1,48 @@
+
+
+#include <conio.h>
+using namespace std;
+
+const int M = 3;
+const int N = 3;
+
+void swap(int* a, int* b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void reverseArray(int arr[M][N])
+{
+	for (int i = 0; i < M; i++) {
+
+		// Initialise start and end index
+		int start = 0;
+		int end = N - 1;
+		while (start < end) {
+
+			// Swap the element
+			swap(&arr[i][start],
+				&arr[i][end]);
+			start++;
+			end--;
+		}
+	}
+
+	for (int i = 0; i < M; i++) {
+		for (int j = 0; j < N; j++) {
+			cout << arr[i][j] << ' ';
+		}
+		cout << endl;
+	}
+}
+
+int main()
+{
+	int arr[][3] = { { 1, 2, 3 },
+					{ 4, 5, 6 },
+					{ 7, 8, 9 } };
+	reverseArray(arr);
+	return 0;
+}
